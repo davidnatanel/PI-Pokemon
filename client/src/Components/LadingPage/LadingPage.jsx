07b67ea8fade1692b43afe563b56e078244e4443
davidnatanel <85590345+davidnatanel@users.noreply.github.com>
2022-08-14 15:50:55 -0300

    fix/responsive

--- client/src/Components/LadingPage/LadingPage.jsx
+++ client/src/Components/LadingPage/LadingPage.jsx
@@ -7,32 +7,33 @@ import pokemon from '../LadingPage/video/pokemon.png'
 import PikachuRun from '../LadingPage/video/PikachuRun.gif'
 import left from '../LadingPage/video/left.png'
 import right from '../LadingPage/video/right.png'
+import poke from '../../img/background/Pokeball.svg'
 
 
 
 function LadingPage(props) {
     return (
         <div className={style.container}>
-        <video className={style.video} src={video} loop autoPlay muted type='video/mp4' />
-        <video className={style.videoCopia} src={video} loop autoPlay muted type='video/mp4' />
+            
+    <img className={style.poke} src={poke} alt="" />
+<Link to='Home'>
+
+<button className={style.button} >Press Start</button>
+</Link>
 
 
+        {/* <video className={style.video} src={video} loop autoPlay muted type='video/mp4' /> */}
+        {/* <video className={style.videoCopia} src={video} loop autoPlay muted type='video/mp4' /> */}
+
+{/* 
         <img src={PikachuRun} className={style.PikachuRun} alt="" />
 
-
-            {/* <div><img src={left}  className={style.left} alt="" /></div>
-            <div><img src={rigth} className={style.rigth} alt="" /></div> */}
-           
            <img src={right} className={style.right}alt="" />
            <img src={left} className={style.left} alt="" />
-            <img src={pokemon} className={style.pokemon} alt="" />
+          <img src={pokemon} className={style.pokemon} alt="" />
 
-            <Link to='Home'>
-
-                <button className={style.button} ><img src={start} className={style.start} alt="" /></button>
-
-            </Link>
-            
+       
+             */}
      
             
             </div>
